28e30659fdda0f7bd02e009b9105c22ad56819c4
Erin-Christensen <sonofchristensen@gmail.com>
2018-08-05 18:52:41 -0400

    fixed tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :email, presence: true
+  
   def admin?
     role == "admin"
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,31 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "validations" do
     let!(:user) { FactoryBot.build(:user) }
-    #let!(:user1) { FactoryBot.build(:user, first_name: "") }
-    #let!(:user2) { FactoryBot.build(:user, last_name: "") }
-    let!(:user3) { FactoryBot.build(:user, email: "") }
-    #let!(:user4) { FactoryBot.build(:user, bio: "") }
-    #let!(:user5) { FactoryBot.build(:user, profile_photo: "") }
-
-    # it "is not valid without a first name" do
-    #   expect(user1).to_not be_valid
-    # end
-
-    # it "is not valid without a last name" do
-    #   expect(user2).to_not be_valid
-    # end
+    let!(:user3) { FactoryBot.build(:user, email: nil) }
 
     it "is not valid without an email" do
       expect(user3).to_not be_valid
     end
-
-    # it "is valid without a bio" do
-    #   expect(user4).to be_valid
-    # end
-
-    # it "is valid without a profile photo" do
-    #   expect(user5).to be_valid
-    # end
 
     it "is valid with valid attributes" do
       expect(user).to be_valid
