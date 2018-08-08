6ffa126e3d2271145eb6e8a16da0e8d7161e6174
Erin-Christensen <sonofchristensen@gmail.com>
2018-08-08 14:50:17 -0400

    add-devise-permissions-for-character-index

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::CharactersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    characters = Character.all
+    characters = Character.where(user: current_user.id)
     render json: characters
   end
 end
